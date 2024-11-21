97d3d1a4e795f483b335018b271a2e36490760d5
clarsen-007 <52528227+clarsen-007@users.noreply.github.com>
2024-11-22 00:29:59 +0200

    Update task_scheduler.sh

--- task_scheduler.sh
+++ task_scheduler.sh
@@ -51,9 +51,8 @@ VERIFY_VERSION() {
 
    $( which wget ) $SCRIPTPATH -P $TEMPFOLDER/
 
-   if [[ "$VERSION" == "$( cat /tmp/task_scheduler.sh | grep 'VERSION=' | cut -d '=' -f2 | grep -Ev '^$' | $( which tr ) -d '[:blank:]' )" ]]
+   if [[ "$VERSION" != "$( cat /tmp/task_scheduler.sh | grep 'VERSION=' | cut -d '=' -f2 | grep -Ev '^$' | $( which tr ) -d '[:blank:]' )" ]]
      then
-     else
         echo -e " \n "
         echo -e " Version is outdated...  Configuring new version."
         echo -e " \n "
